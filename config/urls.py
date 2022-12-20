4613ee45bf70b1fa9c97dbebe080ba68aa0b10a6
Victhereum <tamsvictorisaac@gmail.com>
2022-12-20 03:09:36 +0100

    Configuring for render

--- config/urls.py
+++ config/urls.py
@@ -28,18 +28,18 @@ if settings.DEBUG:
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
-urlpatterns += [
-    # API base url
-    path("api/", include("config.api_router")),
-    # DRF auth token
-    path("auth-token/", obtain_auth_token),
-    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
-    path(
-        "api/docs/",
-        SpectacularSwaggerView.as_view(url_name="api-schema"),
-        name="api-docs",
-    ),
-]
+# urlpatterns += [
+#     # API base url
+#     path("api/", include("config.api_router")),
+#     # DRF auth token
+#     path("auth-token/", obtain_auth_token),
+#     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+#     path(
+#         "api/docs/",
+#         SpectacularSwaggerView.as_view(url_name="api-schema"),
+#         name="api-docs",
+#     ),
+# ]
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
